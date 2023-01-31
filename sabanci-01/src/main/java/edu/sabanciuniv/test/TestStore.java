4794d22ddc1b1917e62d92fa4a6c6497475895f6
Koray Guney <koray.guney@hotmail.com>
2023-01-31 21:38:10 +0300

    added crud examples of hibernate

--- sabanci-01/src/main/java/edu/sabanciuniv/test/TestStore.java
+++ sabanci-01/src/main/java/edu/sabanciuniv/test/TestStore.java
@@ -4,6 +4,7 @@ import edu.sabanciuniv.model.Store;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import jakarta.persistence.TypedQuery;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,6 +23,70 @@ public class TestStore {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("mysqlPU");
         EntityManager entityManager = emf.createEntityManager();
 
+        // saveStores(storeList, entityManager);
+        // findAllStores(entityManager);
+        // findByStoreName(entityManager, store1.getStoreName());
+        // findByStoreNameAndSquareMeter(entityManager, store2.getStoreName(), store2.getSquareMeter());
+        // updateStorePhone(entityManager, store3, "5555555555");
+        deleteStore(entityManager, store2);
+    }
+
+    private static void deleteStore(EntityManager entityManager, Store store2) {
+        entityManager.getTransaction().begin();
+
+        Store foundStore = entityManager.createQuery("FROM Store s WHERE s.storeName = :strName", Store.class)
+                .setParameter("strName", store2.getStoreName())
+                .getSingleResult();
+        entityManager.remove(foundStore);
+
+        entityManager.getTransaction().commit();
+        System.out.println("Store deleted");
+    }
+
+    private static void updateStorePhone(EntityManager entityManager, Store store3, String newPhoneNumber) {
+        entityManager.getTransaction().begin();
+
+        Store foundStore = entityManager.createQuery("FROM Store s WHERE s.storeName = :strName", Store.class)
+                .setParameter("strName", store3.getStoreName())
+                .getSingleResult();
+        foundStore.setStorePhone(newPhoneNumber);
+        entityManager.merge(foundStore);
+
+        entityManager.getTransaction().commit();
+        System.out.println("Store phone updated to : " + newPhoneNumber);
+    }
+
+    private static void findByStoreName(EntityManager entityManager, String storeName) {
+        // 01-alternative
+        // TypedQuery<Store> storeJpql = entityManager.createQuery("FROM Store s WHERE s.storeName = :strName", Store.class);
+        // storeJpql.setParameter("strName", storeName);
+        // Store foundStore = storeJpql.getSingleResult();
+
+        // 02-alternative
+        Store foundStore = entityManager.createQuery("FROM Store s WHERE s.storeName = ?1", Store.class)
+                .setParameter(1, storeName).getSingleResult();
+        System.out.println(foundStore);
+    }
+
+    private static void findByStoreNameAndSquareMeter(EntityManager entityManager, String storeName, Double sqrMeters) {
+        // 02-alternative
+        Store foundStore = entityManager.createQuery("FROM Store s WHERE s.storeName = ?1 AND s.squareMeter = ?2", Store.class)
+                .setParameter(1, storeName)
+                .setParameter(2, sqrMeters)
+                .getSingleResult();
+        System.out.println(foundStore);
+    }
+
+    private static void findAllStores(EntityManager entityManager) {
+        TypedQuery storeJpql = entityManager.createQuery("FROM Store s", Store.class);
+        List<Store> storeList = storeJpql.getResultList();
+
+        for (Store store : storeList) {
+            System.out.println(store);
+        }
+    }
+
+    private static void saveStores(List<Store> storeList, EntityManager entityManager) {
         for (Store store : storeList) {
             entityManager.getTransaction().begin();
             entityManager.persist(store);
