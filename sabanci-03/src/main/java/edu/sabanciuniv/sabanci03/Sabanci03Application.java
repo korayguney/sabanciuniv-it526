5fe55ffacd7cb6fc5649dd43411a43c20fc8b305
Koray Güney <koray.guney@hotmail.com>
2023-02-18 12:03:22 +0300

    initialized first spring boot project

---
+++ sabanci-03/src/main/java/edu/sabanciuniv/sabanci03/Sabanci03Application.java
@@ -0,0 +1,13 @@
+package edu.sabanciuniv.sabanci03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sabanci03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sabanci03Application.class, args);
+	}
+
+}
---
+++ sabanci-03/src/test/java/edu/sabanciuniv/sabanci03/Sabanci03ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.sabanciuniv.sabanci03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sabanci03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
