f0adc7cf507886f00a08e5289df5c6524c55059b
Koray Güney <koray.guney@hotmail.com>
2023-02-23 20:02:51 +0300

    added missing files

---
+++ sabanci-04/src/test/java/edu/sabanciuniv/sabanci04/Sabanci04ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.sabanciuniv.sabanci04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sabanci04ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
