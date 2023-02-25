02362549ee112e653efe3e56ebd7132da0dd4ece
Koray Güney <koray.guney@hotmail.com>
2023-02-25 18:22:00 +0300

    added CRUD operations for Book entity

---
+++ sabanci-05/src/main/java/edu/sabanciuniv/sabanci05/Sabanci05Application.java
@@ -0,0 +1,13 @@
+package edu.sabanciuniv.sabanci05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sabanci05Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sabanci05Application.class, args);
+    }
+
+}
