8ee2279dce48e5220153c006d78c5204a330a33e
Koray Güney <koray.guney@hotmail.com>
2023-02-25 18:24:51 +0300

    added CRUD operations for Book entity

---
+++ sabanci-05/src/test/java/edu/sabanciuniv/sabanci05/Sabanci05ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.sabanciuniv.sabanci05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sabanci05ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
